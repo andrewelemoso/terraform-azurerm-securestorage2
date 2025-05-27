8dfc83c78ed301ad0f7857c58470425d52122421
andrew elemoso <aelemoso@gmail.com>
2025-05-27 16:12:23 +0000

    fixed

--- variables.tf
+++ variables.tf
@@ -6,10 +6,16 @@ variable "location" {
   type = string
 }
 
-variable "name" {
+variable "storage_account_name" {
   type = string
 }
 
-variable "environment" {
+variable "account_tier" {
   type = string
+  default = "Standard"
+}
+
+variable "account_replication_type" {
+  type = string
+  default = "GRS"
 }
